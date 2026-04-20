7489d9f37f0f93f4495e6371482b3adca5cb410e
adamdspencer77 <58069848+adamdspencer77@users.noreply.github.com>
2026-04-20 11:09:10 +0100

    Update mail merge limit to use the number of email addresses found instead of rows #2055
    * Working prototype. #2055
    
    * Tidied. #2055

--- src/main.py
+++ src/main.py
@@ -5084,10 +5084,10 @@ class mailmerge(JSONEndpoint):
         mergeparams = ""
         if post["mergeparams"] != "": mergeparams = asm3.utils.json_parse(post["mergeparams"])
         rows, cols = asm3.reports.execute_query(dbo, post.integer("mergereport"), o.user, mergeparams)
-        count = len(rows)
+        count = len(self.recipients(rows))
         if asm3.utils.is_smcom_smtp(dbo):
             asm3.smcom.check_bulk_email(dbo, count)
-        elif len(rows) > asm3.configuration.mail_merge_max_emails(dbo):
+        elif count > asm3.configuration.mail_merge_max_emails(dbo):
             raise asm3.utils.ASMError(f"{count} exceeds configured limit of {asm3.configuration.mail_merge_max_emails(dbo)} emails via mail merge")
         fromadd = post["from"]
         subject = post["subject"]
